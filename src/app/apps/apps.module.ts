78d9018470cad89a13b36af059d0c39300fdb2d3
Abbas <airekeola@byteworks.com.ng>
2019-10-05 15:38:58 +0100

    Commit

--- src/app/apps/apps.module.ts
+++ src/app/apps/apps.module.ts
@@ -4,23 +4,12 @@ import { AppsListComponent } from './apps-list/apps-list.component';
 import { RouterModule } from '@angular/router';
 import { OthersRoutes } from './apps.routing';
 import { FormsModule } from '@angular/forms';
-import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatInputModule } from '@angular/material/input';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ChartsModule } from 'ng2-charts';
 import { FileUploadModule } from 'ng2-file-upload';
 import { CommonPipesModule } from 'app/pipes/common/common-pipes.module';
+import { MaterialModule } from 'app/material.module';
+
 
 
 
@@ -29,23 +18,11 @@ import { CommonPipesModule } from 'app/pipes/common/common-pipes.module';
   imports: [
     CommonModule,
     FormsModule,
-    MatListModule,
-    MatIconModule,
-    MatButtonModule,
-    MatCardModule,
-    MatMenuModule,
-    MatSlideToggleModule,
-    MatGridListModule,
-    MatChipsModule,
-    MatCheckboxModule,
-    MatRadioModule,
-    MatTabsModule,
-    MatInputModule,
-    MatProgressBarModule,
     FlexLayoutModule,
     ChartsModule,
     FileUploadModule,
     CommonPipesModule,
+    MaterialModule,
     RouterModule.forChild(OthersRoutes)
 
 
